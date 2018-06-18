c49ea24c5a878c7722d008f054dc7208359b4efd
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-18 16:41:27 -0400

    started over

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
-  $.ajax('.searchbutton').click(function(){
-    $.ajax({
-      url: 'http://api.musicgraph.com/',
-      datatype: 'jsonp',
-      success: function(data) {
-        $('.band').html('<class="col-md-4">'+data.current_observation.band);
-        $('.album').html('<class="col-dmd-2">'+data.current_observation.album);
-        $('.song').html('<class="col-md-3">'+data.current_observation.song);
-    })
-  })
-  });
+  $('.button').click(function({
+    console.log("hello");
+  }));
+
+
+var Trackster = {};
+
+/*
+  Given an array of track data, create the HTML for a Bootstrap row for each.
+  Append each "row" to the container in the body to display all tracks.
+*/
+Trackster.renderTracks = function(tracks) {
+
+};
+
+/*
+  Given a search term as a string, query the LastFM API.
+  Render the tracks given in the API query response.
+*/
+Trackster.searchTracksByTitle = function(title) {
+
+};
+});
