041ed56ae8c00bd72685617ee07d89dab5b796a6
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-18 14:41:56 -0400

    MInor js fix

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
       url: 'http://ws.audioscrobbler.com/2.0/',
       datatype: 'jsonp',
       success: function(data) {
-        $('.band').html('<class="col-md-4">'+data.current_observation.weather);
-        $('.album').html('<class="col-dmd-2">'+data.current_observation.weather);
-        $('.song').html('<class="col-md-3">'+data.current_observation.weather );
+        $('.band').html('<class="col-md-4">'+data.current_observation.band);
+        $('.album').html('<class="col-dmd-2">'+data.current_observation.album);
+        $('.song').html('<class="col-md-3">'+data.current_observation.song);
     })
   })
   });
