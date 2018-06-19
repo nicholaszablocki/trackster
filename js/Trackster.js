8d3625069835e57b6892a049d2f7e9cc633aaa88
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-19 15:16:35 -0400

    ohgod

--- js/Trackster.js
+++ js/Trackster.js
@@ -12,7 +12,29 @@ $(document).ready(function(){
     Append each "row" to the container in the body to display all tracks.
   */
   Trackster.renderTracks = function(tracks) {
-
+    var i=0;
+    var length= tracks.length;
+    for (var i=0; i<tracks.length; i++){
+       var information=
+        '<div class="row">'+
+          '<div class="col-md-6">'+
+           '<a href="https://youtu.be/eI_O5_tJ1hA">'+
+             '<i class="far fa-play-circle" style="color:purple;">'+
+             '</i></a>'+
+           tracks[i].name+
+         '</div>'+
+         '<div class="col-md-2">'+
+           'THE RAINCOATS'+
+         '</div>'+
+         '<div class="col-md-2'+
+           'PREMIERE'+
+         '</div>'+
+         '<div class="col-md-2">'+
+           '1 BILLION+'+
+         '</div>+'+
+        '</div>'
+        $('#tracklistcontainer').append(information);
+    }
 };
 
 
@@ -26,8 +48,8 @@ $(document).ready(function(){
   Trackster.searchTracksByTitle = function(title) {
     $.ajax({
       url: "https://ws.audioscrobbler.com/2.0/?method=track.search&track="+title+"&api_key="+API_KEY+"&format=json",
-      success: function(data){
-        console.log(data)
+      success: function(response){
+        console.log(response)
       }
     });
   };
