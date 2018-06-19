23a536e9d406df05dd64451e8fd08e6a85050732
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-18 22:40:36 -0400

    Fresh start

--- js/Trackster.js
+++ js/Trackster.js
@@ -1,30 +1,19 @@
 $(document).ready(function(){
-  const API_KEY='32912447447300ee595e9b667ec0b80d';
-  $('#button').click(function(){
-    var track=$("#input").val();
-    Trackster.searchTracksByTitle($("#input").val());
-    $.ajax({
-      url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track='+track+'&api_key='+API_KEY+'&format=json',
-      datatype: 'jsonp',
-      success: function(data) {
-        
-    })
-  });
-var Trackster = {};
+  var Trackster = {};
 
-/*
-  Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks.
-*/
-Trackster.renderTracks = function(tracks) {
+  /*
+    Given an array of track data, create the HTML for a Bootstrap row for each.
+    Append each "row" to the container in the body to display all tracks.
+  */
+  Trackster.renderTracks = function(tracks) {
 
-};
+  };
 
-/*
-  Given a search term as a string, query the LastFM API.
-  Render the tracks given in the API query response.
-*/
-Trackster.searchTracksByTitle = function(title) {
+  /*
+    Given a search term as a string, query the LastFM API.
+    Render the tracks given in the API query response.
+  */
+  Trackster.searchTracksByTitle = function(title) {
 
-};
+  };
 });
