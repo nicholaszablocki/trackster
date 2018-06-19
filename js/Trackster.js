03b91e73a1d4afb9a7165355267edef86d7db39a
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-18 22:12:57 -0400

    oh god this is so hard

---
+++ js/Trackster.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  const API_KEY='32912447447300ee595e9b667ec0b80d';
+  $('#button').click(function(){
+    var track=$("#input").val();
+    Trackster.searchTracksByTitle($("#input").val());
+    $.ajax({
+      url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track='+track+'&api_key='+API_KEY+'&format=json',
+      datatype: 'jsonp',
+      success: function(data) {
+        $('.band').html('<class="col-md-4">'+data.current_observation.band);
+        $('.album').html('<class="col-dmd-2">'+data.current_observation.album);
+        $('.song').html('<class="col-md-3">'+data.current_observation.song);
+    })
+  });
+var Trackster = {};
+
+/*
+  Given an array of track data, create the HTML for a Bootstrap row for each.
+  Append each "row" to the container in the body to display all tracks.
+*/
+Trackster.renderTracks = function(tracks) {
+
+};
+
+/*
+  Given a search term as a string, query the LastFM API.
+  Render the tracks given in the API query response.
+*/
+Trackster.searchTracksByTitle = function(title) {
+
+};
+});
