7c1c966009855532a7aa2252877724a0de54d638
Nicholas Zablocki <nicholaszablocki@hotmail.com>
2018-06-18 22:14:22 -0400

    please kill me

--- js/Trackster.js
+++ js/Trackster.js
@@ -7,9 +7,7 @@ $(document).ready(function(){
       url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track='+track+'&api_key='+API_KEY+'&format=json',
       datatype: 'jsonp',
       success: function(data) {
-        $('.band').html('<class="col-md-4">'+data.current_observation.band);
-        $('.album').html('<class="col-dmd-2">'+data.current_observation.album);
-        $('.song').html('<class="col-md-3">'+data.current_observation.song);
+        
     })
   });
 var Trackster = {};
